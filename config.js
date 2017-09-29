d17fdb9d5be89682848169e1c4320515354f5bfd
ysKim <znxkznxk1030@gmail.com>
2017-09-29 17:24:35 +0900

    commit for AWS EC2 service

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {
+        port: 3000,
+        secret: 'secret',
+        redisUrl: 'redis://localhost',
+        salt: 'VeritasLuxMea(SNUMiManJob)',
+        routes: {
+                login: '/login',
+                logout: '/logout'
+        },
+        crypto:{
+                workFactor: 5000,
+                keylen: 32,
+                randomSize: 256
+        }
+};
+
+module.exports = config;
+
