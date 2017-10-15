08b8ea0969c8743589e2147648af5086f00c8cf9
ysKim <znxkznxk1030@gmail.com>
2017-10-15 16:45:40 +0900

    CREATE View Col

---
+++ controller/users.js
@@ -0,0 +1,20 @@
+var connection = require('../database/db');
+
+var nicknameCheck = function(req, res){
+
+    var nickName = req.param.nickName;
+    var sql = "SELECT * FROM user WHERE user.displayName = ?";
+    connection.query(sql, [nickName], function(err, profile){
+        if (err) throw err;
+
+        if(profile.displayName){
+            res.json({code:1, message : "this name is already existed!"});
+        }else{
+            res.json({code:0, message : "possible to use"});
+        }
+
+    })
+
+};
+
+exports.nicknameCheck = nicknameCheck;
