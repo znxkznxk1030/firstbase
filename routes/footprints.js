bb3611dc7a61876513b938ecfa154c7f8609f489
ysKim <znxkznxk1030@gmail.com>
2017-10-20 21:58:34 +0900

    CREATE View Col

--- routes/footprints.js
+++ routes/footprints.js
@@ -10,7 +10,7 @@ router.get('/list/:user_id', controller.getFootprintListByUser);
 router.get('/listbylocation/', controller.getFootprintListByLocation);
 router.get('/list/:lat/:lng/:level', controller.getFootprintListByCurrentLocationAndViewLevel);
 
-router.get('/detail', controller.getFootprintByFootprintID);
+router.get('/detail', auth.passMiddleware, controller.getFootprintByFootprintID);
 router.get('/delete/:footprint_id', controller.deleteFootprintByFootprintID);
 
 
