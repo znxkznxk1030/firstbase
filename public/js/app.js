57f8acbc00ec9c674757b51304a67ce655ba8667
SungHwan Yoon <sparkupnova@gmail.com>
2017-10-20 21:41:41 +0900

    마커, 게시물 실시간 변동.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 
+var baseUrl = "http://ec2-13-124-219-114.ap-northeast-2.compute.amazonaws.com:8080";
+
 var mapWidth = $("#map-area").outerWidth();
 var mapHeight = $("#map-area").outerHeight();
 
@@ -12,7 +14,7 @@ $(window).resize(function() {
 
 //지도 생성시에 옵션을 지정할 수 있습니다.
 var map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng(37.3595704, 127.105399), //지도의 초기 중심 좌표
+        center: new naver.maps.LatLng(37.504479, 127.048941), //지도의 초기 중심 좌표
         zoom: 10, //지도의 초기 줌 레벨
         minZoom: 2, //지도의 최소 줌 레벨 //축소
         maxZoom: 12, //확대
@@ -29,31 +31,7 @@ var map = new naver.maps.Map('map', {
 //setOptions 메서드를 통해 옵션을 조정할 수도 있습니다.
 map.setOptions("minZoom", 2);
 
-
-var marker1 = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.3595704, 127.105399),
-    map: map
-});
-
-var marker2 = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.3495704, 127.105399),
-    map: map
-});
-
-var marker3 = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.3695704, 127.105399),
-    map: map
-});
-
-var marker4 = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.3595704, 127.085399),
-    map: map
-});
-
-var marker5 = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.3595704, 127.125399),
-    map: map
-});
+var markers = [];
 
 $(document).ready(function(){
     $.ajax({
@@ -61,33 +39,7 @@ $(document).ready(function(){
         data: "",
         url: '/footprint/list',
         success: function(data) {
-            // var myJSON = JSON.stringify(data);
-            var cnt = Math.min(data.length, 5);
-            var $list = $('<div class="content-wrapper"></div>').appendTo($('#contents'));
-
-            //data[i] 또는 foreach(data)
-            var fnAppendPage = function(start){
-                $('.content-wrapper').html("");
-                var end = Math.min(data.length, start+5);
-                for(var i=start; i<end; i++) {
-                    var o = data[i];
-                    var $content = $('<div class="content"></div>').appendTo($list);
-                    var $title = $('<div class="title"></div>').appendTo($content).text(o.title);
-                    var $post = $('<div class="post"></div>').appendTo($content).text(o.content);
-                }
-            };
-
-            var $page = $('<div class="contents-page"></div>').appendTo($('#contents'));
-
-            var pageCnt = data.length / 5;
-            for(i=0; i<pageCnt; i++) {
-                $('<span class="page"></span>').data('page', i).text(i+1).appendTo($page).click(function(){
-                    var pageIdx = $(this).data('page');
-                    fnAppendPage(pageIdx * 5);
-                });
-            }
-
-            fnAppendPage(0);
+            makePage(data);
         }
     });
 
@@ -98,21 +50,114 @@ $(document).ready(function(){
     var Bounds = map.getBounds();
     getMarkers(Bounds);
     naver.maps.Event.addListener(map, 'bounds_changed', function(bounds) {
-        Bounds = bounds;
-        getMarkers(Bounds);
+        getMarkers(bounds);
     });
+    search_positon();
 });
 
-function getMarkers(Bounds) {
-    alert(Bounds);
-    /*$.ajax({
-        type: 'GET',
-        data: Bounds,
-        url: '/footprint/listbylocation/',
-        success: function(data) {
-            alert(data);
+function makeMarkers(data){
+    if(data.length > 0) {
+        for(var i=0; i<data.length; i++) {
+            var o = data[i];
+            var latitude = o.latitude;
+            var longitude = o.longitude;
+
+            var marker = new naver.maps.Marker({
+                map: map,
+                position: new naver.maps.LatLng(latitude, longitude)
+            });
         }
-    });*/
+    }
+}
+
+function showMarker(map, marker) {
+    if (marker.setMap()) return;
+    marker.setMap(map);
+}
+
+function makePage(data) {
+    // var myJSON = JSON.stringify(data);
+    var cnt = Math.min(data.length, 5);
+    var $list = $('<div id="content-wrapper"></div>').appendTo($('#contents'));
+
+    //data[i] 또는 foreach(data)
+    var fnAppendPage = function(start){
+        $('#content-wrapper').html("");
+        var end = Math.min(data.length, start+5);
+        for(var i=start; i<end; i++) {
+            var o = data[i];
+            var $content = $('<div class="content"></div>').appendTo($list);
+            var $title = $('<div class="title"></div>').appendTo($content).text(o.title);
+            var $post = $('<div class="post"></div>').appendTo($content).text(o.content);
+        }
+    };
+
+    var $page = $('<div id="contents-page"></div>').appendTo($('#contents'));
+
+    var pageCnt = data.length / 5;
+    for(i=0; i<pageCnt; i++) {
+        $('<span class="page"></span>').data('page', i).text(i+1).appendTo($page).click(function(){
+            var pageIdx = $(this).data('page');
+            fnAppendPage(pageIdx * 5);
+        });
+    }
+
+    fnAppendPage(0);
+}
+
+function renderPage(data) {
+    if(data.length > 0) {
+        var cnt = Math.min(data.length, 5);
+        $('#content-wrapper').html("");
+        $('#contents-page').html("");
+        var fnAppendPage = function(start){
+            var end = Math.min(data.length, start+5);
+            for(var i=start; i<end; i++) {
+                var o = data[i];
+                var $content = $('<div class="content"></div>').appendTo($("#content-wrapper"));
+                var $title = $('<div class="title"></div>').appendTo($content).text(o.title);
+                var $post = $('<div class="post"></div>').appendTo($content).text(o.content);
+            }
+        };
+        var pageCnt = data.length / 5;
+        for(i=0; i<pageCnt; i++) {
+            $('<span class="page"></span>').data('page', i).text(i+1).appendTo($("#contents-page")).click(function(){
+                var pageIdx = $(this).data('page');
+                fnAppendPage(pageIdx * 5);
+            });
+        }
+
+        fnAppendPage(0);
+    }
+    makeMarkers(data);
+}
+
+function getMarkers(Bounds) {
+
+    var pickBounds = _.pick(Bounds,"_min","_max");
+
+
+    var startlat = pickBounds._min._lat;
+    var startlng = pickBounds._min._lng;
+    var endlat = pickBounds._max._lat;
+    var endlng = pickBounds._max._lng;
+
+    var ajaxData = {
+        startlat : endlat,
+        startlng : startlng,
+        endlat : startlat,
+        endlng : endlng
+    }
+    console.log(ajaxData);
+
+    $.ajax({
+        type: 'GET',
+        data: ajaxData,
+        url: baseUrl+'/footprint/listbylocation/',
+        success: function(data) {
+            renderPage(data);
+        }
+    });
 }
 
 function write_button_click() {
@@ -197,7 +242,7 @@ function search_positon(){
 
 function onSuccessGeolocation(position) {
     var location = new naver.maps.LatLng(position.coords.latitude,
-       position.coords.longitude);
+     position.coords.longitude);
 
     map.setCenter(location); // 얻은 좌표를 지도의 중심으로 설정합니다.
     map.setZoom(10); // 지도의 줌 레벨을 변경합니다.
