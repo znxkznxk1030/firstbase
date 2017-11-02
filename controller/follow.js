99607884ac3be40e3bdfa60546e392ec1acf9895
ysKim <znxkznxk1030@gmail.com>
2017-11-02 11:04:12 +0900

    follow api 수정

--- controller/follow.js
+++ controller/follow.js
@@ -28,7 +28,7 @@ var getFollowerList = function(req, res){
     }
 
     const sqlGetFollowerList =
-        "SELECT * " +
+        "SELECT follower_id AS followId, target_id AS targetId " +
         "FROM user INNER JOIN follow " +
         "ON user.id = follow.target_id " +
         "WHERE user.displayName = ? ";
@@ -52,7 +52,7 @@ var getFollowingList = function(req, res){
     }
 
     const sqlGetFollowerList =
-        "SELECT * " +
+        "SELECT follower_id AS followId, target_id AS targetId " +
         "FROM user INNER JOIN follow " +
         "ON user.id = follow.follower_id " +
         "WHERE user.displayName = ? ";
@@ -62,7 +62,6 @@ var getFollowingList = function(req, res){
 
         return res.status(200).json({code: 1, followers : followers, message: '팔로잉한 유저들 찾기 성공'});
     })
-
 
 };
 
