c55023c053c7030e60b38b78c726021e74e8f567
ysKim <znxkznxk1030@gmail.com>
2017-11-03 17:49:20 +0900

    채팅방 공사중

--- chat.js
+++ chat.js
@@ -5,7 +5,7 @@ var jwt = require("jsonwebtoken");
 
 var SECRET = config.token_secret;
 
-mongoose.connect('mongodb://localhost:27017', {
+mongoose.connect('mongodb://127.0.0.1:27017', {
     useMongoClient: true
 }, function(err){
     if(err){
