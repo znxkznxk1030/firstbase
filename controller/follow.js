a8cd44d3a5fbdbc736955dbf04cf827add4d486e
ysKim <znxkznxk1030@gmail.com>
2017-11-14 23:00:54 +0900

    팔로우 팔로워

--- controller/follow.js
+++ controller/follow.js
@@ -1,6 +1,7 @@
 var connection = require('../database/db');
 var async = require('async');
 var getImageUrl = require("./files").getImageUrl;
+var _ = require('underscore');
 
 var unfollow = function(req, res){
     const id = req.user.id,
@@ -11,6 +12,12 @@ var unfollow = function(req, res){
 
     const sqlUnFollow =
         "DELETE FROM follow WHERE follower_id = ? AND target_id = ?";
+
+    const sqlNumFollower =
+        "SELECT count(follower_id) AS numFollowers FROM follow WHERE follower_id = ?";
+
+    const sqlNumFollowing =
+        "SELECT count(follower_id) AS numFollowings FROM follow WHERE following_id = ? ";
 
     var task = [
         function(cb){
@@ -25,17 +32,32 @@ var unfollow = function(req, res){
                 function(err, result){
                     if(err) return cb(err, null);
 
-                    return cb(null);
+                    return cb(null, targetId);
                 });
+        },
+        function(targetId, cb){
+            connection.query(sqlNumFollower, [targetId], function(err, numFollowers){
+                if(err) return cb(err, null);
+
+                return cb(null, {targetId : targetId,
+                numFollowers: JSON.parse(JSON.stringify(numFollowers))[0].numFollowers});
+            });
+        },
+        function(result, cb){
+            connection.query(sqlNumFollowing, [result.targetId], function(err, numFollowings){
+                if(err) return cb(err, null);
+
+                delete result.targetId;
+                return cb(null, _.extend(result, {numFollowings: JSON.parse(JSON.stringify(numFollowings))[0].numFollowings}));
+            });
         }
     ];
 
     async.waterfall(task, function(err, result){
         if(err) return res.status(400).json({code: -1, message: '팔로우 오류'});
         else{
-            return res.status(200).json({code: 1, message:'팔로우 시작'});
+            return res.status(200).json(_.extend(result, {code: 1, message:'팔로우 시작'}));
         }
-
     });
 
 };
@@ -44,6 +66,9 @@ var follow = function(req, res){
 
     const id = req.user.id,
         targetDisplayName = req.body.targetDisplayName;
+
+    const sqlGetId =
+        "SELECT id FROM user WHERE displayName = ?";
 
     const sqlFollow =
         "INSERT INTO follow (follower_id, target_id) " +
@@ -54,11 +79,14 @@ var follow = function(req, res){
         "FROM follow " +
         "WHERE follower_id = ? AND target_id = ? ";
 
-    const sqlGetId =
-        "SELECT id FROM user WHERE displayName = ?";
-
     const sqlUnFollow =
         "DELETE FROM follow WHERE follower_id = ? AND target_id = ?";
+
+    const sqlNumFollower =
+        "SELECT count(follower_id) AS numFollowers FROM follow WHERE follower_id = ?";
+
+    const sqlNumFollowing =
+        "SELECT count(follower_id) AS numFollowings FROM follow WHERE following_id = ? ";
 
 
     var task = [
@@ -78,17 +106,34 @@ var follow = function(req, res){
                             function (err, result) {
                                 if (err) return cb('팔로우 오류', null);
 
-                                return cb(null, false);
+                                return cb(null, {targetId: targetId,
+                                    isFollow:false});
                             });
                     }else{
                         connection.query(sqlFollow, [id, targetId],
                             function(err, result){
                                 if(err) return cb('팔로우 오류', null);
 
-                                return cb(null, true);
+                                return cb(null, {targetId: targetId,
+                                    isFollow:true});
                             });
                     }
                 }
+            });
+        },
+        function(result, cb){
+            connection.query(sqlNumFollower, [result.targetId], function(err, numFollowers){
+                if(err) return cb(err, null);
+
+                return cb(null, _.extend(result,{numFollowers: JSON.parse(JSON.stringify(numFollowers))[0].numFollowers}));
+            });
+        },
+        function(result, cb){
+            connection.query(sqlNumFollowing, [result.targetId], function(err, numFollowings){
+                if(err) return cb(err, null);
+
+                delete result.targetId;
+                return cb(null, _.extend(result, {numFollowings: JSON.parse(JSON.stringify(numFollowings))[0].numFollowings}));
             });
         }
     ];
