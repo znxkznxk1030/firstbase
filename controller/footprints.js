62b889012f074f313db4513e1f0243b099b2e462
ysKim <znxkznxk1030@gmail.com>
2017-11-24 18:01:34 +0900

    link footprint -> 구현

--- controller/footprints.js
+++ controller/footprints.js
@@ -790,6 +790,7 @@ var getLinkMarker = function (req, res) {
         "ON footprint.footprint_id = comment.footprint_id " +
         "WHERE footprint.footprint_id IN (" +
         "SELECT linked_footprint_id FROM link WHERE link_footprint_id = ? " +
+        "ORDER BY created_date " +
         ")" +
         "GROUP BY footprint_id ";
 
