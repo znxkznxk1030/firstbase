a218cd30462bf7adff72e938235ec0049162fdc7
ysKim <znxkznxk1030@gmail.com>
2017-11-25 14:54:32 +0900

    유저정보 수정제한

--- routes/users.js
+++ routes/users.js
@@ -11,72 +11,75 @@ router.use(passport.session());
 router.get('/', auth.authenticate);
 
 
-
-router.get('/login-form', function(req, res, next){
-        res.render('login');
+router.get('/login-form', function (req, res, next) {
+    res.render('login');
 });
 
-router.get('/signup-form', function(req, res, next) {
+router.get('/signup-form', function (req, res, next) {
     res.render('registration');
 });
 
-
-
-router.post('/registrate', controller.isFormVaild,function(req, res, next){
+router.post('/registrate', controller.isFormVaild, function (req, res, next) {
     console.log(req.body);
 
-    if(req.body.password1 === req.body.password2){
-        controller.registrateUser(req.body, function(err, result){
-           if(err) return res.status(401).json({code : -1, message: err});
-            else return res.status(200).json({code: 1, message : 'success register'});
+    if (req.body.password1 === req.body.password2) {
+        controller.registrateUser(req.body, function (err, result) {
+            if (err) return res.status(401).json({code: -1, message: err});
+            else return res.status(200).json({code: 1, message: 'success register'});
         });
-    }else{
-        return res.status(401).json({code:-1, message: 'Two password fields are not matched!'})
+    } else {
+        return res.status(401).json({code: -1, message: 'Two password fields are not matched!'})
     }
 
 });
 
-router.post('/login', function(req, res, next){
-    passport.authenticate('local-login', function(err, user, info){
-    var error = err || info;
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local-login', function (err, user, info) {
+        var error = err || info;
 
-    if(error)
-        return res.status(401)
-            .json(error);
-    if(!user) return res.status(404)
-        .json({code: -2,
-            message: 'user not found...'});
+        if (error)
+            return res.status(401)
+                .json(error);
+        if (!user) return res.status(404)
+            .json({
+                code: -2,
+                message: 'user not found...'
+            });
 
-    console.log(user);
+        console.log(user);
 
-    var token = auth.signToken(user);
-    res.cookie('jwt', token).json({code: 1, message: 'success to login', accessToken: token, displayName: user.displayName});
+        var token = auth.signToken(user);
+        res.cookie('jwt', token).json({
+            code: 1,
+            message: 'success to login',
+            accessToken: token,
+            displayName: user.displayName
+        });
 
     })(req, res, next);
 });
 
-router.get('/login-failure', function(req, res){
+router.get('/login-failure', function (req, res) {
     console.log('failure');
-    res.json({ message : "failure to login"});
+    res.json({message: "failure to login"});
 });
 
-router.get('/login-success', function(req, res){
+router.get('/login-success', function (req, res) {
     console.log('success');
-    res.json({ message : "success to login"});
+    res.json({message: "success to login"});
 });
 
-router.get('/logout', function(req, res){
+router.get('/logout', function (req, res) {
     req.logout();
     res.clearCookie('jwt')
-    res.json({ message : "success to logout"});
+    res.json({message: "success to logout"});
 });
-
 
 
 router.get('/nickname-check', controller.nicknameCheck);
 
 router.get('/profile', auth.authMiddleware, controller.getUserInfoByReqHeader);
-router.post('/update', auth.authMiddleware, controller.isFormVaild, controller.updateUserInfo);
+router.post('/update', auth.authMiddleware,  controller.updateUserInfo);
 router.post('/updateImage', auth.authMiddleware, controller.updateUserImage);
 
 router.get('/detail', auth.authMiddleware, controller.getUserInfoByUserDisplayName);
